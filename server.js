a81730f5f5cab39931fe4ced81f9b5db91f2ce18
Gedalya Krycer <gedalya@krycer.com>
2020-08-06 15:40:00 -0700

    Added comments to unfamiliar lines of code, explaining what they did

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
+// This helps run our server connection and routes
 const express = require("express");
+
+// This middleware helps with showing useful express information in the terminal 
 const logger = require("morgan");
+
+// This provides structure, schema validation and manages relationships for MongoDB
 const mongoose = require("mongoose");
+
+// This allows us to use gZip
 const compression = require("compression");
 
 const PORT = 3000;
 
 const app = express();
 
+
 app.use(logger("dev"));
 
+// This activates gZip
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Allows us to refrence our public folder
 app.use(express.static("public"));
 
+// connects Mongoose to our database
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
