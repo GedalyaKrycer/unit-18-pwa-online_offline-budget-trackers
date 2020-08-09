7d42b3a260aeff22b0890e780e54d3df54ed0cb0
Gedalya Krycer <gedalya@krycer.com>
2020-08-09 15:38:44 -0700

    Added indexDB code

--- models/transaction.js
+++ models/transaction.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// This is a new schema profile/model that says a name, value and date must be specified. The name should be a string and should be trimmed. The value must be a number. The date can be left blank and by deafult it will add the current date.
 const transactionSchema = new Schema(
   {
     name: {
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connects Mongoose to our database
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
